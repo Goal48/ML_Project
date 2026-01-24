4f95c226151f1529d62af5f6b07c4860d45ec1d7
Pius Dutta <piusduttanitdgpbt@gmail.com>
2026-01-24 15:29:08 +0530

    transform changes1

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,5 +1,16 @@
+import os
 import sys
 from dataclasses import dataclass
 
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer    
+
+from src.exception import CustomException
+from src.logger import logging
+
+class DataTransformationConfig:
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
